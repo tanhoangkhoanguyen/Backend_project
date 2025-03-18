15eb7b7212c35724b7ce07311594bc97ad5c44df
tanhoangkhoanguyen <hoangkhoa.nguyentan@gmail.com>
2025-03-18 14:59:03 -0400

    Add MongoDB

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parsedr';
 
 import { PORT } from './config/env.js';
 
@@ -6,13 +7,20 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subcriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from '.middlewares/error.middleware.js'
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded( { extended: false }));
+app.use(cookieParser());
 
 // api/v1/auth/sign-up
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subcriptions', subcriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Subcription Tracker API!');
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 
+import { signUp, signIn, signOut } from '../controllers/auth.controller.js';
+
 const authRouter = Router();
 
-authRouter.post( '/sign-up', (req, res) => res.send( { title: 'Sign up' }));
-authRouter.post( '/sign-in', (req, res) => res.send( { title: 'Sign in' }));
-authRouter.post( '/sign-out', (req, res) => res.send( { title: 'Sign out' }));
+// - /api/v1/auth/sign-up -> POST BODY -> { name, email, password } -> CREATES A NEW USER
+
+// Path: /api/v1/auth/sign-up (POST)
+authRouter.post( '/sign-up', signUp);
+authRouter.post( '/sign-in', signIn);
+authRouter.post( '/sign-out', signOut);
 
 export default authRouter;
